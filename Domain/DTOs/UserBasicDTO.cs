e1f4fcd92e2e648c94700ad33b361883462099e8
IuliaIspas3 <iuliaispas3@gmail.com>
2023-11-16 09:19:42 +0100

    userbasicdto class

---
+++ Domain/DTOs/UserBasicDTO.cs
@@ -0,0 +1,13 @@
+namespace Domain.DTOs;
+
+public class UserBasicDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public string? Name { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Type { get; set; }
+}
